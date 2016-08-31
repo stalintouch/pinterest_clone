6e2730439f29071ac6b0cb9145d72bcc7739fa8a
stalintouch <stalintouch1983@gmail.com>
2016-08-30 21:51:44 -0400

    Add act as votable

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  acts_as_votable
 end
